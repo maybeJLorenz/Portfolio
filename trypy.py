70acfcd15ff735b5c442cb134605f5fb874d3a5c
maybeJLorenz <167827887+maybeJLorenz@users.noreply.github.com>
2024-10-23 17:51:25 -0700

    adding python file

---
+++ trypy.py
@@ -0,0 +1,15 @@
+## tying a python file for this assignment
+
+
+print("========= python program =======")
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print("Hi" + name + ", I'm your VScode computer.")
+
+if(age):
+    print("I see you are " + age + " years old. Cheers to a good year.")
+else:
+    cantDrink = 21 - age
+    print("I see you are" + age + " years old." + cantDrink + "more year(s) until 21 hehe.")
